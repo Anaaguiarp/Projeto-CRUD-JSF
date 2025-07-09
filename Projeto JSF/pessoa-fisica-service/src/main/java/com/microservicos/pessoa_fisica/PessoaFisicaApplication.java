1b68522e32cb03205e2eda2bcf403590990bd173
gabrielestudos1503 <gabrielribeiro1503@edu.unifil.br>
2025-07-09 12:32:13 -0300

    Reestruturando os micros

---
+++ Projeto JSF/pessoa-fisica-service/src/main/java/com/microservicos/pessoa_fisica/PessoaFisicaApplication.java
@@ -0,0 +1,13 @@
+package com.microservicos.pessoa_fisica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PessoaFisicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PessoaFisicaApplication.class, args);
+	}
+
+}
---
+++ Projeto JSF/pessoa-fisica-service/src/test/java/com/microservicos/pessoa_fisica/PessoaFisicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservicos.pessoa_fisica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PessoaFisicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
